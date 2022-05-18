00902143499ff83575dee8633c53759dd48de0e3
cmattosr <cmrocha@gmail.com>
2022-05-17 22:59:56 -0300

    tabuada

--- 02_tabuada.py
+++ 02_tabuada.py
@@ -1,13 +1,24 @@
 """Imprime a tabuada do 1 ao 10"""
-__version__ = "0.1.0"
+__version__ = "1.1"
 __author__ = "Cesar Mattos Rocha"
 
+template_base = """
+---Tabuada do 2---
+        
+   {bloco_texto:^18}
+
+##################
+"""
+
 # numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-numeros = list(range(1,11))
+numeros = list(range(1, 11))
 
 # Iterable (percorrível)
-for numero in numeros:
-    print("Tabuada do: ", numero)
-    for outro_numero in numeros:
-        print(numero * outro_numero)
-    print("----------------------")    
+for n1 in numeros:
+    print("{:-^18}".format(f"Tabuada do {n1}"))
+    print()
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
+        
+    print("#" * 18)
--- interpolacao.py
+++ interpolacao.py
@@ -2,13 +2,14 @@ email_tmpl = """
 Olá, %(nome)s,
 
 Tem interesse em comprar %(produto)s?
-Este produto é ótimo para resolver %(texto)s
+Este produto é ótimo para %(texto)s
 Clique agora em %(link)s
 Apenas %(quantidade)d disponíveis!
 Preço promocional: %(preco).2f
 """
 
 clientes = ["Maria", "João", "Bruno"]
+preco = [344.1, 85.45, 984.77]
 
-for cliente in clientes:
-    print(email_tmpl % {"nome": cliente, "produto": "caneta", "texto": "Escrever muito bem", "link": "www.canetaslegais.com.br", "quantidade": 1, "preco": 58.23})
+for i, cliente in enumerate(clientes):
+    print(email_tmpl % {"nome": cliente, "produto": "caneta", "texto": "escrever muito bem", "link": "www.canetaslegais.com.br", "quantidade": 1, "preco": preco[i]})
