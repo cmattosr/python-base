19f43645f64f8c59d795dec41b93115265bbc0fb
cmattosr <cmrocha@gmail.com>
2022-05-26 21:30:24 -0300

    infixcalc

--- 05_Exercicio_Escola__com_dicionarios.py
+++ 05_Exercicio_Escola__com_dicionarios.py
@@ -5,7 +5,7 @@
 Imprimir a lista de crianças agrupadas por sala
 que frequentas cada uma das atividades.
 """
-__version__ = "0.1.1"
+__version__ = "0.1.3"
 
 ########################################################
 #      ATENçÃO: MODIFIQUE ESSE CÓDIGO!                 #
@@ -13,32 +13,28 @@ __version__ = "0.1.1"
 ########################################################
 
 # Dados
-sala1 = ["Erik", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"]
-sala2 = ["Joao", "Antonio", "Carlos", "Maria", "Isolda"]
+dados = {
+    "sala1": ["Erik", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"],
+    "sala2": ["Joao", "Antonio", "Carlos", "Maria", "Isolda"],
+}
 
-aula_ingles = ["Erik", "Maia", "Joana", "Carlos", "Antonio"]
-aula_musica = ["Erik", "Carlos", "Maria"]
-aula_danca = ["Gustavo", "Sofia", "Joana", "Antonio"]
+atividades = {
+    "Inglês": ["Erik", "Maia", "Joana", "Carlos", "Antonio"],
+    "Música": ["Erik", "Carlos", "Maria"],
+    "Dança": ["Gustavo", "Sofia", "Joana", "Antonio"],
+}
 
-atividades = [
-    ("Inglês", aula_ingles),
-    ("Música", aula_musica),
-    ("Dança", aula_danca),
-]
-
-# Listar alunos em cada atividade por sala
-
-for nome_atividade, atividade in atividades:
-
+for nome_atividade, aluno in atividades.items():
+    
     print(f"Alunos da atividade {nome_atividade}\n")
     print("-" * 40)
 
-    # sala1 que tem interseção com a atividade
-    atividade_sala1 = set(sala1) & set(atividade)
-    atividade_sala2 = set(sala2).intersection(atividade)
+    atividade_sala1 = set(dados["sala1"]) & set(aluno)
+    atividade_sala2 = set(dados["sala2"]) & set(aluno)
 
-    print("Sala1 ", atividade_sala1)
-    print("Sala2 ", atividade_sala2)
+    print("sala1", atividade_sala1)
+    print("Sala2", atividade_sala2)
 
     print()
     print("#" * 40)
+    print()
